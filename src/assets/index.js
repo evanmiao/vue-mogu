64154ebbd71dc4b7059823e73044c4f506753dc7
evan <evan.c.miao@gmail.com>
2020-03-26 23:45:32 +0800

    update config

--- src/assets/index.js
+++
@@ -1,4 +0,0 @@
-import './styles/reset.css'
-import './styles/rem.css'
-import './iconfont/iconfont.css'
-import './styles/global.styl'
--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,5 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/vue-echo/'
-    : '/',
-  css: {
-    sourceMap: false,
-    modules: false,
-    loaderOptions: {
-      stylus: {
-        // 全局引入 index.styl 文件
-        import: [path.resolve(__dirname, './src/assets/styles') + '/index.styl']
-      }
-    }
-  }
+    ? '/vue-mogu/'
+    : '/'
 }
