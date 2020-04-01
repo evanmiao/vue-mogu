f0f52bc1f9456a29e4da2db8e9ed61282da43cc3
Evan Miao <evan.c.miao@gmail.com>
2020-04-01 23:56:51 +0800

    tabBar component

---
+++ src/components/base/TabBar/TabBar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="tab-bar">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TabBar'
+}
+</script>
+
+<style lang="stylus" scoped>
+  #tab-bar
+    position fixed
+    left 0
+    right 0
+    bottom 0
+
+    display flex
+    text-align center
+
+    height 49px
+    border-top 1px solid #eee
+    box-shadow 0px -1px 1px rgba(150,150,150,.08)
+    background-color #f6f6f6
+</style>
---
+++ src/views/home/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <main-tab-bar></main-tab-bar>
+  </div>
+</template>
+
+<script>
+import MainTabBar from '@/components/layout/MainTabBar'
+
+export default {
+  name: 'Home',
+  data() {
+    return {
+    }
+  },
+  methods: {
+  },
+  components: {
+    MainTabBar
+  }
+}
+</script>
